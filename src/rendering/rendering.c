7554a6babd3103670eae67c2aec5faa47fde808c
SmokedSeaSalt <mathijsrijn@gmail.com>
2025-10-06 14:35:38 +0200

    refactored rendering to use function pointers

--- src/rendering/rendering.c
+++ src/rendering/rendering.c
@@ -6,66 +6,39 @@
 /*   By: mvan-rij <mvan-rij@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 09:57:43 by egrisel           #+#    #+#             */
-/*   Updated: 2025/10/06 11:19:43 by mvan-rij         ###   ########.fr       */
+/*   Updated: 2025/10/06 14:30:50 by mvan-rij         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <float.h>
 #include "structs.h"
 #include "math_inc.h"
 #include "rendering.h"
 
-
-
-void	calculate_sphere_hit(t_ray *ray, t_sphere *sphere)
-{
-	float disc;
-
-	//todo sometime later: add the check for hit behind vector to set is_hit 0
-	disc = sphere_intersects(*ray, *sphere);
-	if (ray->results.is_hit == 0)
-	{
-		if (disc < 0)
-		ray->results.is_hit = 0;
-		else
-		ray->results.is_hit = 1;
-	}
-}
-
 void	loop_though_objects(t_ray *ray, t_object *object_list)
 {
+	int		is_hit;
+	float	hit_dist;
 
+	ray->results.hit_dist = FLT_MAX;
 	while (object_list != NULL)
 	{
-		if (object_list->type == SPHERE)
-			calculate_sphere_hit(ray, object_list->data);
-		// if (object_list->type == PLANE)
-
-		// if (object_list->type == CYLINDER)
-
-
+		is_hit = object_list->is_hit(ray, object_list->data);
+		if (is_hit == 1)
+		{
+			hit_dist = object_list->get_hit_dist(ray, object_list->data);
+			if (hit_dist > 0.0f && hit_dist < ray->results.hit_dist)
+			{
+				ray->results.is_hit = 1;
+				ray->results.is_hit = hit_dist;
+				ray->results.object = object_list;
+			}
+		}
 		object_list = object_list->next;
 	}
+	if (ray->results.is_hit == 1)
+		ray->results.object->get_hit_data(ray, ray->results.object->data);
 }
-
-//void	loop_though_objects(t_ray *ray, t_object *object_list)
-//{
-//	int is_hit;
-//	float hit_dist;
-
-//	while (object_list != NULL)
-//	{
-//		is_hit = object_list.is_hit();
-//		if ( is_hit == 1)
-//		{
-//			hit_dist = object_list.get_dist();
-//			if (hit_dist < ray->results.hit_dist);
-//				ray->results.obj = object_list;
-//		}
-//		object_list = object_list->next;
-//	}
-//	ray->results.obj.get_result_data;
-
-//}
 
 
 void render_pixel(int x, int y, t_scene *scene)
